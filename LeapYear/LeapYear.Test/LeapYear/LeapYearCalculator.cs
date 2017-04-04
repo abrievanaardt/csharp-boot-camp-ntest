569ee107d2aa6e36e6304967b64b757d99d68554
Michelle Swanepoel <michelle.swanepoel@vcontractor.co.za>
2017-04-04 16:01:15 +0200

    implemented leap year

--- LeapYear/LeapYear.Test/LeapYear/LeapYearCalculator.cs
+++ LeapYear/LeapYear.Test/LeapYear/LeapYearCalculator.cs
@@ -6,6 +6,11 @@ namespace LeapYear
     {
         public bool IsLeapYear(long year)
         {
+            if((year % 4 == 0 && year % 100 == 0 && year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+            {
+                    return true;
+            }
+
             return false;
         }
     }
