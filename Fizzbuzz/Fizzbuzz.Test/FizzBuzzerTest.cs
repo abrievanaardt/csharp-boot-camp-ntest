11f075b05af761ae0263aeee5d21ec620548303f
Abrie van Aardt <abrie.vanaardt@vcontractor.co.za>
2017-04-04 13:36:15 +0200

    Added Fizzbuzzer class
    Signed-off-by: Abrie van Aardt <abrie.vanaardt@vcontractor.co.za>

--- Fizzbuzz/Fizzbuzz.Test/FizzBuzzerTest.cs
+++ Fizzbuzz/Fizzbuzz.Test/FizzBuzzerTest.cs
@@ -3,10 +3,15 @@ using NUnit.Framework;
 
 namespace Fizzbuzz.Test
 {
+    [SetUpFixture]
     public class FizzbuzzerTest
     {
         IFizzbuzzer _fizzbuzzer;
 
-
+        [SetUp]
+        public void setup()
+        {
+            _fizzbuzzer = new Fizzbuzzer();
+        }
     }
 }
---
+++ Fizzbuzz/Fizzbuzz/Fizzbuzzer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizzbuzz
+{
+    public class Fizzbuzzer : IFizzbuzzer
+    {
+        string IFizzbuzzer.getFizzbuzz(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        string IFizzbuzzer.PrintFizzbuzzSequence(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
